09bb81cd2b1bd37ac60004e02ff157cb95289ae8
AMJones <am@jonesiscoding.com>
2022-06-09 14:59:53 -0500

    Adds PleasingDartSassFilter.php

---
+++ src/PleasingDartSassFilter.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * PleasingSassFilter.php
+ */
+
+namespace XQ\Pleasing\Filter;
+
+use XQ\Drivers\AbstractSassDriver;
+use XQ\Drivers\DartSassDriver;
+use XQ\Pleasing\Filter\Options\SassSourceMapInterface;
+use XQ\Pleasing\Filter\Options\SassSourceMapTrait;
+
+/**
+ * Loads SASS/SCSS files using a driver for Dart Sass
+ *
+ * @author  Aaron M Jones <am@jonesiscoding.com>
+ * @version Pleasing Filters v2.0 (https://github.com/jonesiscoding/pleasing-filters)
+ * @license MIT (https://github.com/jonesiscoding/pleasing-filters/blob/master/LICENSE)
+ * @package XQ\Pleasing\Filter
+ */
+class PleasingDartSassFilter extends AbstractSassDriverFilter implements SassSourceMapInterface
+{
+  use SassSourceMapTrait;
+
+  /** @var DartSassDriver */
+  protected $_Driver;
+
+  /**
+   * @param bool $sourceMap
+   *
+   * @return PleasingDartSassFilter|AbstractSassDriverFilter
+   */
+  public function setSourceMap( bool $sourceMap ): AbstractSassDriverFilter
+  {
+    $this->Driver()->setSourceMap( $sourceMap );
+
+    return $this;
+  }
+
+  /**
+   * @param string $tmp
+   *
+   * @return AbstractSassDriverFilter
+   */
+  public function setTmpPath( string $tmp ): AbstractSassDriverFilter
+  {
+    // Reset the SASS Driver, as it may depend on this path
+    $this->_Driver = null;
+
+    return parent::setTmpPath($tmp);
+  }
+
+  // region //////////////////////////////////////////////// Implemented Methods
+
+  /**
+   * @return DartSassDriver|AbstractSassDriver
+   * @throws \Exception
+   */
+  protected function Driver(): AbstractSassDriver
+  {
+    if ( !$this->_Driver )
+    {
+      $this->_Driver = new DartSassDriver($this->getDebugLevel(), $this->getBin(), $this->getTmpPath());
+    }
+
+    return $this->_Driver;
+  }
+
+  // endregion ///////////////////////////////////////////// End Implemented Methods
+}
